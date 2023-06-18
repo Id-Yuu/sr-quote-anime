3dd0d1b5b672aaf9af575c7ceeffe663d1610df8
Yuu <122996864+Id-Yuu@users.noreply.github.com>
2023-06-19 04:57:18 +0700

    edit btn

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,37 +1,13 @@
 import React from "react";
 
-const stylesBtn = {
-  textDecoration: "none",
-  position: "fixed",
-  bottom: "10px",
-  right: "60px",
-  display: "block",
-  padding: "10px 20px",
-  borderRadius: "10px",
-  color: "white",
-  backgroundColor: "black",
-};
-
-const styleRefresh = {
-  textDecoration: "none",
-  position: "fixed",
-  bottom: "10px",
-  right: "10px",
-  display: "block",
-  padding: "10px 20px",
-  borderRadius: "10px",
-  color: "white",
-  backgroundColor: "black",
-};
-
 export const Button = (props) => {
   const { text, onClicks } = props;
   return (
     <>
-      <button style={stylesBtn} type="button" onClick={onClicks}>
+      <button type="button" onClick={onClicks}>
         {text}
       </button>
-      <a href="/" style={styleRefresh}>
+      <a href="/">
         #
       </a>
     </>
